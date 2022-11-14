b223b8590a761be46a00fdf0aebf444b679490ed
Md Musfiqur Rahman <113307668+musfiqurR661@users.noreply.github.com>
2022-11-14 20:52:15 +0600

    Add files via upload

---
+++ scan/src/Main.java
@@ -0,0 +1,14 @@
+package x.news;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        //input
+        Scaner sc = new Scaner(System.in);
+        String name =sc.nextLine();
+        System.out.println(name);
+    }
+}
